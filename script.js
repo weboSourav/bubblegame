2902229ea406c928d557cc183a163d672388eb2d
unknown <winterfell118@gmail.com>
2025-08-06 21:55:43 +0530

    Updated bubble game with timer and layout fixes

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-function makeBubble(){
-let clutter = "";
-for(var i = 1; i<=160;i++){
-    var rn = Math.floor(Math.random()*10)
- clutter += `<div class="bubble">${rn}</div>`;
+var timer = 10;
+var score = 0;
+var hitrn = 0;
+function increaseScore(){
+score += 10;
+document.querySelector("#scoreval").textContent = score;
+
+
+
+
+
+}
+function getNewHit(){
+
+hitrn = Math.floor(Math.random()*10);
+document.querySelector("#hitval").textContent = hitrn;
+
+
 
 }
 
-document.querySelector("#pbtm").innerHTML = clutter;
-}
-var timer = 60;
-function runTimer(){
-setInterval(function(){
-    if(timer>0){
-timer--;
+
+
+function makeBubble() {
+    let clutter = "";
+    for (var i = 1; i <= 160; i++) {
+        var rn  = Math.floor(Math.random() * 10); // random 0–9
+        clutter += `<div class="bubble">${rn}</div>`;
     }
-document.querySelector("#timerval").textContent = timer;
-},1000);
-
-
-
-
-
+    document.querySelector("#pbtm").innerHTML = clutter;
 }
+
+
+
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click" , function(details){
+
+var clickedNum = (Number(details.target.textContent));
+if(clickedNum === hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+}
+
+
+})
+
 makeBubble();
 runTimer();
+getNewHit();
